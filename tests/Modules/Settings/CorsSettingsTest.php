813bcebd6368af615079286af716b99776ed58e7
nicumicle <20170987+nicumicle@users.noreply.github.com>
2021-06-08 21:17:30 +0200

    [master] Write tests for CorsSettings

---
+++ tests/Modules/Settings/CorsSettingsTest.php
@@ -0,0 +1,88 @@
+<?php
+namespace SimpleJWTLoginTests\Modules\Settings;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use SimpleJWTLogin\Modules\Settings\CorsSettings;
+use SimpleJWTLogin\Modules\WordPressDataInterface;
+
+class CorsSettingsTest extends TestCase
+{
+    private $wordPressData;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->wordPressData = $this->getMockBuilder(WordPressDataInterface::class)
+                                    ->getMock();
+        $this->wordPressData->method('sanitizeTextField')
+                            ->willReturnCallback(
+                                function ($parameter) {
+                                    return $parameter;
+                                }
+                            );
+    }
+
+    public function testProperties(){
+        $post = [
+            'cors' => [
+                'enabled' => '1',
+                'allow_origin_enabled' => '1',
+                'allow_origin' => 'test',
+                'allow_methods_enabled' => '1',
+                'allow_methods' => 'POST',
+                'allow_headers_enabled' => '1',
+                'allow_headers' => 'Header'
+            ]
+        ];
+
+        $corsSettings = (new CorsSettings())
+            ->withWordPressData($this->wordPressData)
+            ->withSettings([])
+            ->withPost($post);
+        $corsSettings->initSettingsFromPost();
+        $corsSettings->validateSettings();
+        $this->assertSame(
+            true,
+            $corsSettings->isCorsEnabled()
+        );
+        $this->assertSame(
+            true,
+            $corsSettings->isAllowOriginEnabled()
+        );
+        $this->assertSame(
+            'test',
+            $corsSettings->getAllowOrigin()
+        );
+        $this->assertSame(
+            true,
+            $corsSettings->isAllowHeadersEnabled()
+        );
+        $this->assertSame(
+            'Header',
+            $corsSettings->getAllowHeaders()
+        );
+        $this->assertSame(
+            true,
+            $corsSettings->isAllowMethodsEnabled()
+        );
+        $this->assertSame(
+            'POST',
+            $corsSettings->getAllowMethods()
+        );
+    }
+
+    public function testValidation(){
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Cors is enabled but no option is checked. Please check at least one option.');
+        $corsSettings = (new CorsSettings())
+            ->withWordPressData($this->wordPressData)
+            ->withSettings([])
+            ->withPost(['cors' => [
+                'enabled' => 1
+            ]]);
+        $corsSettings->initSettingsFromPost();
+        $corsSettings->validateSettings();
+
+    }
+}

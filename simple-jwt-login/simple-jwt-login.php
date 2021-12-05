ec2e94e017a7e0ee4f2dbbc346340f5635f6f07b
nicumicle <20170987+nicumicle@users.noreply.github.com>
2021-12-05 10:22:42 +0100

    Create release

--- simple-jwt-login/simple-jwt-login.php
+++ simple-jwt-login/simple-jwt-login.php
@@ -7,7 +7,7 @@
     Author URI: https://profiles.wordpress.org/nicu_m/
     Text Domain: simple-jwt-login
     Domain Path: /i18n
-    Version: 3.4.0
+    Version: 3.4.1
 */
 
 use SimpleJWTLogin\Modules\SimpleJWTLoginSettings;

3769fff64a518f76c3526a0d64f04f1a9fdb3821
nicumicle <20170987+nicumicle@users.noreply.github.com>
2022-04-27 07:01:40 +0200

    Update plugin version

--- simple-jwt-login/simple-jwt-login.php
+++ simple-jwt-login/simple-jwt-login.php
@@ -7,7 +7,7 @@
     Author URI: https://profiles.wordpress.org/nicu_m/
     Text Domain: simple-jwt-login
     Domain Path: /i18n
-    Version: 3.4.5
+    Version: 3.4.6
 */
 
 use SimpleJWTLogin\Modules\SimpleJWTLoginSettings;

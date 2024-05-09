fb7942eb5bd332ba73c6449c7f1eb69c5deb232f
nicumicle <20170987+nicumicleI@users.noreply.github.com>
2024-05-09 10:31:16 +0200

    Update i18n

--- simple-jwt-login/views/delete-view.php
+++ simple-jwt-login/views/delete-view.php
@@ -188,7 +188,9 @@ if (! defined('ABSPATH')) {
         <p class="text-muted">
 			<?php echo __('You can use `.` (dot) as a separator for sub-array values.', 'simple-jwt-login'); ?>
             <br/>
-			<?php echo __('Example: Use `user.id` for getting key `id` from array `user`', 'simple-jwt-login'); ?>
+			<?php echo __('Example: Use `id` for getting the `id` key from the JWT payload.', 'simple-jwt-login'); ?>
+            <br />
+			<?php echo __('Example: Use `user.id` for getting the `id` key from the array `user`', 'simple-jwt-login'); ?>
         </p>
     </div>
 </div>
